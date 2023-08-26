4a072ea8412acf2784ef0238063560e27604672d
vingrax <visal.biju04@gmail.com>
2023-08-26 12:11:05 +0530

    Redirection Included

--- urlshortener/urls.py
+++ urlshortener/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('authentication.urls'))
+    path('',include('authentication.urls')),
+    path('',RedirectView.as_view(url='login/'))
+
 
 ]
